eb67860f47088142381ab36bca99bfb2c9471b69
Phil Ross <philippross369@gmail.com>
2016-07-15 18:48:07 +0000

    upgraded configurations

---
+++ before.R
@@ -0,0 +1,14 @@
+
+# Commands ----------------------------------------------------------------
+
+# clear workspace
+rm(list = ls(all = TRUE))
+
+
+# Functions ---------------------------------------------------------------
+
+sshhh <- function(a.package){                                                                                                                                                                               
+  numb
+  suppressWarnings(suppressPackageStartupMessages(
+    library(a.package, character.only=TRUE)))
+}

aace5304276bdd3cfe213b91f2d5e2f1ac24eb0b
Phil Ross <philippross369@gmail.com>
2016-07-19 17:50:58 +0000

    finished lesson 9

--- knitcitations.R
+++ knitcitations.R
@@ -3,5 +3,7 @@ options("citation_format" = "pandoc")
 
 citep("10.1038/nature09442")             # Liu_2010
 citep("10.1038/ncomms4346")              # Liu_2014
+citep("10.1186/1475-2875-12-328")        # Pacheco_2013
+citep("10.1017/S0031182010001575")       # SILVA_2010
 
 write.bibtex(file = "knitcitations.bib")
